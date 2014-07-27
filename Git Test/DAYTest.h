f4b8f3cf85a670656846dc032c4b460226fc0fac
Daniel Day <DDay@Daniels-MacBook-Pro.local>
2014-07-27 18:59:03 -0400

    added a test class

---
+++ Git Test/DAYTest.h
@@ -0,0 +1,13 @@
+//
+//  DAYTest.h
+//  Git Test
+//
+//  Created by Daniel Day on 7/27/14.
+//  Copyright (c) 2014 Daniel Day. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DAYTest : NSObject
+
+@end
---
+++ Git Test/DAYTest.m
@@ -0,0 +1,13 @@
+//
+//  DAYTest.m
+//  Git Test
+//
+//  Created by Daniel Day on 7/27/14.
+//  Copyright (c) 2014 Daniel Day. All rights reserved.
+//
+
+#import "DAYTest.h"
+
+@implementation DAYTest
+
+@end
